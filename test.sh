c161e13f1a9759878680881172406489d6eab876
Anmol Sharma <anmolsharma0234@gmail.com>
2024-11-16 19:07:43 +0530

    fix: add missing files for chapter 2
    Signed-off-by: Anmol Sharma <anmolsharma0234@gmail.com>

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 pip install jupyterlab
-jupyter nbconvert --execute ./Chapter1.ipynb --to script
-jupyter nbconvert --execute ./Chapter2.ipynb --to script
+jupyter nbconvert --execute ./chapter-1/Chapter1.ipynb --to script
+jupyter nbconvert --execute ./chapter-2/Chapter2.ipynb --to script
